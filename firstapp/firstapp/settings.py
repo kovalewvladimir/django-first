8c1e8a400c4a87fe1bf5ab956309ad9eaf7887cd
Vladimir <kovalew.vladimir@gmail.com>
2016-09-01 20:24:07 +0300

    Ldap авторизация

--- firstapp/firstapp/settings.py
+++ firstapp/firstapp/settings.py
@@ -128,5 +128,32 @@ STATICFILES_DIRS = [
 
 
 # LDAP
-import firstapp.private as private
-AUTH_LDAP_SERVER_URI = private.LDAP_SERVER_URI
+AUTHENTICATION_BACKENDS = [
+    'django_auth_ldap.backend.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',]
+
+
+# Ldap auth
+AUTH_LDAP_SERVER_URI = "ldap://192.168.0.30"
+AUTH_LDAP_BIND_DN = "cn=django_user,cn=Users,dc=vld,dc=local"
+AUTH_LDAP_BIND_PASSWORD = "Qwert123"
+AUTH_LDAP_USER_SEARCH = LDAPSearch("cn=Users,dc=vld,dc=local",
+    ldap.SCOPE_SUBTREE, "(sAMAccountName=%(user)s)")
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail",
+}
+
+
+# Search group
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch("cn=Users,dc=vld,dc=local",
+    ldap.SCOPE_SUBTREE, "(objectClass=groupOfNames)")
+AUTH_LDAP_GROUP_TYPE = GroupOfNamesType()
+AUTH_LDAP_REQUIRE_GROUP = "cn=django_group,cn=Users,dc=vld,dc=local"
+
+
+# Logger
+logger = logging.getLogger('django_auth_ldap')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
